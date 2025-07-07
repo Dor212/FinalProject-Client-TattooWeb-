691ca0436faff757396566913e26cc28ed7d83b8
dor <dor@ut.com>
2025-07-07 17:14:13 +0300

    2.7

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dorohana.OmerAvivTattoo',
+  appName: 'OmerAvivTattoo',
+  webDir: 'dist'
+};
+
+export default config;
